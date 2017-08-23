1898890ce78f4f6c71b180c781bc2bdeb814e3d9
Anthony-g2 <ant.greenwell2@gmail.com>
2017-08-22 20:05:31 -0700

    game created

--- js/app.js
+++ js/app.js
@@ -2,5 +2,28 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  let turns = 1;
+
+  $('.box').click(function(){
+    if ($(this).is(':empty')){
+      if (turns % 2 === 0&&($(this).is(':empty'))) {
+        console.log('X\'s turn');
+        $(this).text(`x`);
+      } else if (turns % 2 != 0) {
+        console.log('O\'s turn');
+        $(this).text(`o`);
+      };
+      turns ++;
+    } else if ($(this).is(':empty')===false){
+      turns + 0;
+    };
+  });
+
+  $('.reset').click(function(){
+    $('.box').text(``);
+    turns = 1 ;
+  });
+
+  
 
 });
